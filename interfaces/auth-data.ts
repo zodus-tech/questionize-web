661118da014fe9ca93cafe697a354c568c8f3dff
LucasVieiraS <lucasvieirasilva.lpta@gmail.com>
2024-10-09 21:20:40 -0300

    feat: Add auth

---
+++ interfaces/auth-data.ts
@@ -0,0 +1,12 @@
+export interface LoginData {
+  username: string
+  password: string
+  rememberUser: boolean
+}
+
+export interface RegisterData {
+  fullName: string
+  username: string
+  email: string
+  password: string
+}
---
+++ utils/endpoints.ts
@@ -0,0 +1,2 @@
+export const baseUrl =
+  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080'
