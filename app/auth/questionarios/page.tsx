848537ba25bee2911fa0d47fca77362865805538
Arthur Marques <arthurmarquesoliveira001@gmail.com>
2024-11-12 20:53:54 -0300

    remove/renamed: unnecessary files

--- app/auth/questionarios/page.tsx
+++
@@ -1,5 +0,0 @@
-import QuestionariosPage from './components/wrapper'
-
-export default function Dashboard() {
-  return <QuestionariosPage />
-}
--- interfaces/genre.ts
+++
@@ -1,4 +0,0 @@
-export interface Genre {
-  name: string
-  id: number
-}
