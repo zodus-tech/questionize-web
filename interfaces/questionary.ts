1c514f5e2f963cea687d8612b0b65d42c38436ba
Arthur Marques <arthurmarquesoliveira001@gmail.com>
2024-11-12 20:56:17 -0300

    feat: questionary interfaces

--- interfaces/questionary.ts
+++ interfaces/questionary.ts
@@ -1,7 +1,30 @@
+export enum QuestionType {
+  MULTIPLE_CHOICE = 'MULTIPLE_CHOICE',
+  BOOLEAN = 'BOOLEAN',
+  TEXT = 'TEXT',
+}
+
+export interface Question {
+  id: number
+  text: string
+  type: QuestionType
+  statistics: unknown
+  options?: string[]
+}
+
+interface Options {
+  startDate: Date
+  endDate: Date
+  answersLimit: number
+  anonymous: boolean
+}
+
 export interface Questionary {
   id: number
   title: string
-  description: string
+  createdAt?: Date
+  options: Options
+  questions: Question[]
 }
 
 export interface QuestionnairesProps {
