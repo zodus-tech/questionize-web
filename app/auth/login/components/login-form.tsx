5049426c01b86115aef9183bcc9f91dcf8548381
LucasVieiraS <lucasvieirasilva.lpta@gmail.com>
2024-11-21 21:33:59 -0300

    refactor: cleanup login page

--- app/auth/login/components/login-form.tsx
+++ app/auth/login/components/login-form.tsx
@@ -4,11 +4,9 @@ import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as z from 'zod'
-import Link from 'next/link'
 import { useAuth } from '@/contexts/auth-context'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { Checkbox } from '@/components/ui/checkbox'
 import {
   Form,
   FormControl,
@@ -17,13 +15,7 @@ import {
   FormLabel,
   FormMessage,
 } from '@/components/ui/form'
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Alert, AlertDescription } from '@/components/ui/alert'
 import { useRouter } from 'next/navigation'
 import Cookies from 'js-cookie'
@@ -108,7 +100,7 @@ export default function LoginForm() {
           'linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0)), url(/placeholder_banner.png)',
       }} */
     >
-      <Card className="w-full max-w-md mx-auto">
+      <Card className="w-full max-w-md mx-auto mb-16">
         <CardHeader>
           <CardTitle className="text-2xl font-bold text-left">Login</CardTitle>
         </CardHeader>
@@ -162,24 +154,6 @@ export default function LoginForm() {
                 )}
               />
 
-              <FormField
-                control={form.control}
-                name="rememberUser"
-                render={({ field }) => (
-                  <FormItem className="flex flex-row items-start space-x-3 space-y-0">
-                    <FormControl>
-                      <Checkbox
-                        checked={field.value}
-                        onCheckedChange={field.onChange}
-                      />
-                    </FormControl>
-                    <div className="space-y-1 leading-none">
-                      <FormLabel>Lembrar Usuário</FormLabel>
-                    </div>
-                  </FormItem>
-                )}
-              />
-
               {error && (
                 <Alert variant="destructive">
                   <AlertDescription>{error}</AlertDescription>
@@ -192,17 +166,6 @@ export default function LoginForm() {
             </form>
           </Form>
         </CardContent>
-        <CardFooter className="flex justify-center">
-          <p className="text-sm text-muted-foreground">
-            Não tem uma conta?{' '}
-            <Link
-              href="/auth/register"
-              className="font-medium text-primary hover:underline"
-            >
-              Registre-se
-            </Link>
-          </p>
-        </CardFooter>
       </Card>
     </div>
   )
