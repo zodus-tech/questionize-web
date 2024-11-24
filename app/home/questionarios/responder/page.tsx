89da24a60a11e58394b50d4d03fae8c601f7b6b1
Arthur Marques <arthurmarquesoliveira001@gmail.com>
2024-11-24 01:50:55 -0300

    remove: add new components; rename files

--- app/home/questionarios/responder/page.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-import QuestionaryResponsePage from './[id]/page'
-
-export default function FormResponse({ params }: { params: { id: string } }) {
-  return <QuestionaryResponsePage params={params} />
-}
