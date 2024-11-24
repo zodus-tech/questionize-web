a029ff0937e40fe97ab8fd68a6f9c5f160ee4e3a
Arthur Marques <arthurmarquesoliveira001@gmail.com>
2024-11-24 01:46:53 -0300

    feat: new delete dialog component

---
+++ components/deleteDialog/index.tsx
@@ -0,0 +1,49 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+import { Button } from '@/components/ui/button'
+import { Trash2 } from 'lucide-react'
+
+interface DeleteDialogProps {
+  handleDelete: () => void
+  element: string
+}
+
+export default function DeleteDialog({
+  handleDelete,
+  element,
+}: DeleteDialogProps) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Button variant="ghost" size="icon">
+          <Trash2 className="h-4 w-4" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent className={`bg-white`}>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Tem certeza que deseja excluir o {element}?
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            Essa ação não poderá ser desfeita.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          <AlertDialogAction onClick={handleDelete}>
+            Continuar
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
