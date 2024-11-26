74adb7d52188a71147645982330323ea807325ee
Arthur Marques <arthurmarquesoliveira001@gmail.com>
2024-11-26 00:17:22 -0300

    feat: searchbar

--- app/admin/questionnaires/components/wrapper.tsx
+++ app/admin/questionnaires/components/wrapper.tsx
@@ -1,8 +1,7 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import { PlusCircle } from 'lucide-react'
-import Link from 'next/link'
+import { PlusCircle, Search } from 'lucide-react' // Importa o ícone de lupa
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Cookies from 'js-cookie'
@@ -11,14 +10,12 @@ import { useRouter } from 'next/navigation'
 import { Questionary } from '@/interfaces/questionary'
 import LoadingSpinner from '@/components/loadingSpinner'
 import Card from '@/components/card'
+import { Input } from '@/components/ui/input'
 
 export default function QuestionnairesPage() {
   const [questionnaires, setQuestionnaires] = useState<Questionary[]>([])
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [searchTerm, setSearchTerm] = useState('')
   const [loading, setLoading] = useState(false)
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [error, setError] = useState<string | null>(null)
-
   const { toast } = useToast()
   const router = useRouter()
 
@@ -43,7 +40,10 @@ export default function QuestionnairesPage() {
         setQuestionnaires(content)
       } catch (error) {
         console.error('Ocorreu um erro ao encontrar os questionários', error)
-        setError('Ocorreu um erro ao encontrar os questionários')
+        toast({
+          title: 'Erro',
+          description: 'Não foi possível carregar os questionários.',
+        })
       } finally {
         setLoading(false)
       }
@@ -98,27 +98,32 @@ export default function QuestionnairesPage() {
         <main className="container sticky top-[56px] z-10 mt-4 px-4 py-4 bg-tile-pattern bg-center bg-repeat rounded-lg w-full max-w-screen-xl">
           <div className="flex justify-between items-center p-2">
             <h2 className="text-2xl font-bold text-white">Questionários</h2>
-            <Link href={'/admin/questionary/create'}>
-              <Button>
-                <PlusCircle className="mr-2 h-4 w-4" />
-                Novo
-              </Button>
-            </Link>
+            <Button>
+              <PlusCircle className="mr-2 h-4 w-4" />
+              Novo
+            </Button>
           </div>
         </main>
+        <div className="mt-4 w-full max-w-screen-xl mx-auto relative">
+          <div className="relative">
+            <Search className="h-4 w-4 absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-400" />
+            <Input
+              placeholder="Buscar questionários..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="pl-10 bg-white"
+            />
+          </div>
+        </div>
         <div className="flex-1 overflow-auto mt-4">
           <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 w-full max-w-screen-xl mx-auto">
-            {questionnaires.length === 0 ? (
-              <div className="w-full max-w-md absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                <p className="text-center font-bold">
-                  Nenhum questionário foi encontrado 😔
-                </p>
-                <p className="text-center text-gray-400 text-sm">
-                  Tente criar um novo...
-                </p>
-              </div>
-            ) : (
-              questionnaires.map((questionary) => (
+            {questionnaires
+              .filter((questionary) =>
+                questionary.title
+                  .toLowerCase()
+                  .includes(searchTerm.toLowerCase()),
+              )
+              .map((questionary) => (
                 <Card
                   key={questionary.id}
                   id={questionary.id}
@@ -130,8 +135,22 @@ export default function QuestionnairesPage() {
                   onDelete={() =>
                     handleDeleteForm(questionary.id, questionary.title)
                   }
+                  element={questionary.title}
                 />
-              ))
+              ))}
+            {questionnaires.filter((questionary) =>
+              questionary.title
+                .toLowerCase()
+                .includes(searchTerm.toLowerCase()),
+            ).length === 0 && (
+              <div className="w-full max-w-md absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <p className="text-center font-bold">
+                  Nenhum questionário foi encontrado 😔
+                </p>
+                <p className="text-center text-gray-400 text-sm">
+                  Tente criar um novo...
+                </p>
+              </div>
             )}
           </div>
         </div>
