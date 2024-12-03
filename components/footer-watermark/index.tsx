eb4262e905fa5b81eea2aa9db670a5de0a08dff9
Arthur Marques <arthurmarquesoliveira001@gmail.com>
2024-12-02 22:51:52 -0300

    feat: new footer watermark component

---
+++ components/footer-watermark/index.tsx
@@ -0,0 +1,7 @@
+export default function Watermark() {
+  return (
+    <footer className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-center text-gray-300 text-sm">
+      zodus@2024 - Unisagrado
+    </footer>
+  )
+}
